506e6853c60f9a1012fa0efd8fe5d5d154871fd3
junfeng2.hu <junfeng.hu2@mapabc.com>
2020-04-05 14:54:20 +0800

    调整核心配置2

--- microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//测试
 @SpringBootApplication
 @EnableEurekaServer
 //开启作为Eureka Server的客户端的支持
